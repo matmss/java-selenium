bd2e1ff6664c4e086c0f2aa265510ca60d04375a
matheusmendes <matheusmendes@matheuss-mbp>
2021-06-07 09:45:24 -0300

    FIlling and sending quote

--- testsite/src/test/java/tests/FormFill.java
+++ testsite/src/test/java/tests/FormFill.java
@@ -25,30 +25,30 @@ public class FormFill {
 		
 		driver.findElement(By.id("make")).click();
 		driver.findElement(By.id("model")).click();
-		driver.findElement(By.id("cylindercapacity")).click();
-		driver.findElement(By.id("engineperformance")).click();
-		driver.findElement(By.id("dateofmanufacture")).click();
+		driver.findElement(By.id("cylindercapacity")).sendkeys("1200");
+		driver.findElement(By.id("engineperformance")).sendkeys("1500");
+		driver.findElement(By.id("dateofmanufacture")).sendkeys("07/12/2021");
 		driver.findElement(By.id("numberofseats")).click();
 		driver.findElement(By.id("righthanddriveyes")).click();
 		driver.findElement(By.id("numberofseatsmotorcycle")).click();
 		driver.findElement(By.id("fuel")).click();
-		driver.findElement(By.id("payload")).click();
-		driver.findElement(By.id("totalweight")).click();
-		driver.findElement(By.id("listprice")).click();
-		driver.findElement(By.id("licenseplatenumber")).click();
-		driver.findElement(By.id("annualmileage")).click();
+		driver.findElement(By.id("payload")).sendkeys("50000");
+		driver.findElement(By.id("totalweight")).sendkeys("75000");
+		driver.findElement(By.id("listprice")).sendkeys("80000");
+		driver.findElement(By.id("licenseplatenumber")).sendkeys("4654678");
+		driver.findElement(By.id("annualmileage")).sendkeys("1000");
 		driver.findElement(By.id("nextenterinsurantdata")).click();
 		
 		driver.findElement(By.id("enterinsurantdata")).click();
 
-		driver.findElement(By.id("firstname")).click();
-		driver.findElement(By.id("lastname")).click();
-		driver.findElement(By.id("birthdate")).click();
+		driver.findElement(By.id("firstname")).sendkeys("matheus");
+		driver.findElement(By.id("lastname")).sendkeys("santos");
+		driver.findElement(By.id("birthdate")).sendkeys("05/12/1992");
 		driver.findElement(By.id("gendermale")).click();
 		driver.findElement(By.id("streetaddress")).click();
 		driver.findElement(By.id("country")).click();
-		driver.findElement(By.id("zipcode")).click();
-		driver.findElement(By.id("city")).click();
+		driver.findElement(By.id("zipcode")).sendkeys("80223000");
+		driver.findElement(By.id("city")).sendkeys("Curitiba");
 		driver.findElement(By.id("speeding")).click();
 		driver.findElement(By.id("website")).click();
 		driver.findElement(By.id("open")).click();
